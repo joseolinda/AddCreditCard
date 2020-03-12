206699ed87924de31f531be96fa41a2fb569c664
José Olinda <joseolindad@yahoo.com.br>
2020-03-12 09:48:53 -0300

    Respostas da Q2
    Respostas da segunda questão

--- js/addcard.js
+++ js/addcard.js
@@ -4,7 +4,7 @@ const hideNumber = document.querySelector('#show');
 
 const toggleTypePasswordToText = function(input) {
     if ( input.type === 'password' )
-        input.type = 'text';
+        input.type = 'number';
     else
         input.type = 'password';
 };
@@ -13,4 +13,25 @@ hideNumber.addEventListener('change', function () {
    for(let i = 0; i < cardNumbers.length; i++) {
        toggleTypePasswordToText(cardNumbers[i]);
    }
+});
+
+// Resposta da Q2
+const verifyIsEmpty = function (input) {
+    return input.value === '' || input.value === undefined;
+};
+
+const allInputs = document.querySelectorAll(
+    'input:not([type=submit]):not([type=checkbox]):not([type=radio])'
+            );
+
+const btnAdicionar = document.querySelector('input[type="submit"].submit');
+
+btnAdicionar.addEventListener('click', function (e) {
+    for( let i = 0; i < allInputs.length; i++) {
+        if (verifyIsEmpty(allInputs[i])) {
+            e.preventDefault()
+            console.log('O formulário possui elementos vazios');
+            break;
+        }
+    }
 });
